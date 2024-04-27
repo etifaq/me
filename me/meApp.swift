88512e4b6462ff0b7e5176e76e8ceb698926502e
etifaq <3soom11jr1@gmail.com>
2024-04-28 01:06:37 +0400

    Initial Commit

---
+++ me/meApp.swift
@@ -0,0 +1,17 @@
+//
+//  meApp.swift
+//  me
+//
+//  Created by A on 28/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
